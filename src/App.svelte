efeb2a4fc171518305b80278fe6cd268519879f2
phptuts <glaserpower@gmail.com>
2022-12-22 12:09:05 -0800

    module2-video4

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,15 @@
 <script>
-  import Fun from './Fun.svelte';
-  import Funny from './Funny.svelte';
+  let name = 'Bob';
+  let favoriteColor = 'red';
+  let favoriteNumber = 30;
+  let fontSize = 20;
+  setTimeout(function () {
+    favoriteColor = 'blue';
+    fontSize = 400;
+  }, 1000);
 </script>
 
 <h1>Hello You Svelte Developer</h1>
-<Fun />
-<Funny />
+<p style="color: {favoriteColor};">{name.toUpperCase()}</p>
+<p>{favoriteNumber / 30}</p>
+<p style="font-size: {fontSize}px;">{fontSize}</p>
