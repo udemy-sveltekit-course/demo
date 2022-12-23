c564187829b8a11d6f1cd2e699b3300101b9bb31
phptuts <glaserpower@gmail.com>
2022-12-23 15:36:16 -0800

    module2-video9

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,50 @@
 <script>
-  let name = 'Ash';
-  let fontSize = 30;
-  let disabled = true;
-  setTimeout(() => {
-    name = 'Carson';
-  }, 1000);
+  let left = 100;
+  let top = 100;
+  function clickMe(e) {
+    console.log(e);
+    alert('Clicked');
+  }
+  function onInput(e) {
+    console.log(e.target.value, 'input');
+  }
+  function onChange(e) {
+    console.log(e, 'change');
+  }
+  function onKeyup(e) {
+    switch (e.key) {
+      case 'ArrowUp':
+        top -= 10;
+        break;
+      case 'ArrowDown':
+        top += 10;
+        break;
+      case 'ArrowLeft':
+        left -= 10;
+        break;
+      case 'ArrowRight':
+        left += 10;
+        break;
+    }
+  }
+  function handleSubmit(e) {
+    console.log('handlesubmit');
+  }
 </script>
 
-<h1>Hello Svelte Developer</h1>
-<input type="range" bind:value={fontSize} />
-<h2 style="font-size: {fontSize}px">Font Size: {fontSize}px</h2>
-<h2>Hello {name}</h2>
-<input type="text" bind:value={name} />
-<button {disabled}>Click Me</button>
+<button on:click|once={clickMe}>Click Me</button>
+<input type="range" on:input={onInput} on:change={onChange} />
+<svelte:window on:keydown={onKeyup} />
+<form on:submit|stopPropagation|preventDefault={handleSubmit}>
+  <button type="submit">Submit Form</button>
+</form>
+<div style="left: {left}px; top: {top}px;" />
+
+<style>
+  div {
+    width: 100px;
+    height: 100px;
+    position: absolute;
+    background-color: aqua;
+  }
+</style>
