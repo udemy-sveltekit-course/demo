94a5867542ae53fdc121fe18556e935b063fcde0
phptuts <glaserpower@gmail.com>
2022-12-23 22:14:24 -0800

    module2-video9

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,18 @@
 <script>
-  let left = 100;
-  let top = 100;
-  function clickMe(e) {
+  import Container from './Container.svelte';
+  import ChildEvent from './ChildEvent.svelte';
+  function onFormSubmit(e) {
     console.log(e);
-    alert('Clicked');
+    const { name, age } = e.detail;
+    console.log(name, age, 'details from event');
   }
-  function onInput(e) {
-    console.log(e.target.value, 'input');
-  }
-  function onChange(e) {
-    console.log(e, 'change');
-  }
-  function onKeyup(e) {
-    switch (e.key) {
-      case 'ArrowUp':
-        top -= 10;
-        break;
-      case 'ArrowDown':
-        top += 10;
-        break;
-      case 'ArrowLeft':
-        left -= 10;
-        break;
-      case 'ArrowRight':
-        left += 10;
-        break;
-    }
-  }
-  function handleSubmit(e) {
-    console.log('handlesubmit');
+  function onCustomEvent(e) {
+    alert(e.detail);
   }
 </script>
 
-<button on:click|once={clickMe}>Click Me</button>
-<input type="range" on:input={onInput} on:change={onChange} />
-<svelte:window on:keydown={onKeyup} />
-<form on:submit|stopPropagation|preventDefault={handleSubmit}>
-  <button type="submit">Submit Form</button>
-</form>
-<div style="left: {left}px; top: {top}px;" />
-
-<style>
-  div {
-    width: 100px;
-    height: 100px;
-    position: absolute;
-    background-color: aqua;
-  }
-</style>
+<Container
+  on:click={() => alert('was clicked')}
+  on:form_submit={onFormSubmit}
+/>
+<ChildEvent on:custom_event={onCustomEvent} />
