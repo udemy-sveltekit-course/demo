9bf739dd74024d0326b6c5656073af8fc9c10d0f
phptuts <glaserpower@gmail.com>
2022-12-25 11:47:59 -0800

    module2-video12

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,18 @@
 <script>
-  let name = 'Joe';
-  $: capitalizedName = name.toUpperCase();
-  let age = 33;
-  $: dogYears = age * 7;
-  let red = 10;
-  let blue = 0;
-  let green = 0;
-  let highGreen = false;
-  $: colorValue = color(red, green, blue);
-  $: console.log(green);
-  $: {
-    console.log(green, 'green');
-    console.log(red, 'red');
-  }
-
-  $: if (green > 100) {
-    highGreen = true;
-  }
-
-  function color(red, green, blue) {
-    return `rgb(${red}, ${green}, ${blue})`;
-  }
-
-  setTimeout(() => {
-    green += 150;
-  }, 1500);
+  let colorName = 'red';
+  let isChecked = false;
 </script>
 
-<h2>{name}</h2>
-<h2>{capitalizedName}</h2>
-
-<input type="text" bind:value={name} />
-
-<h2>Age: {age}</h2>
-<h2>Dog years: {dogYears}</h2>
-<input type="number" bind:value={age} />
-
-<h2>Color Value: {colorValue}</h2>
-<h2>{highGreen ? 'HIGH ' : 'LOW '} Green</h2>
+{#if colorName.toLowerCase() === 'blue'}
+  <h2>Blue</h2>
+{:else if colorName.toLowerCase() === 'red'}
+  <h2>Red</h2>
+{:else}
+  <h2>Some other color</h2>
+{/if}
+<input type="text" bind:value={colorName} />
+<input type="checkbox" bind:checked={isChecked} />
+<hr />
+{#if isChecked}
+  <h2>You win!!!</h2>
+{/if}
