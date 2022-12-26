4315799855a83a538a9b68a924cb875c33867da3
phptuts <glaserpower@gmail.com>
2022-12-26 12:50:08 -0800

    module2-video14

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,18 @@
   let colorList = ['red', 'green', 'blue', 'purple'];
   let color = '';
   function addColor() {
-    if (color !== '') {
+    if (color !== '' && !colorList.includes(color)) {
       colorList = [color, ...colorList];
       color = '';
+    } else {
+      alert('Color must be present and not exist in the list.');
     }
   }
 </script>
 
 <ul>
-  {#each colorList as color}
-    <li>{color}</li>
+  {#each colorList as color, index (color)}
+    <li>{index + 1}. {color}</li>
   {/each}
 </ul>
 
