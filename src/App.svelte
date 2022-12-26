d902ad6ba42caf477761d08f7fd4c92cfd615aaf
phptuts <glaserpower@gmail.com>
2022-12-26 10:10:17 -0800

    module2-video13

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,19 @@
 <script>
-  let colorName = 'red';
-  let isChecked = false;
+  let colorList = ['red', 'green', 'blue', 'purple'];
+  let color = '';
+  function addColor() {
+    if (color !== '') {
+      colorList = [color, ...colorList];
+      color = '';
+    }
+  }
 </script>
 
-{#if colorName.toLowerCase() === 'blue'}
-  <h2>Blue</h2>
-{:else if colorName.toLowerCase() === 'red'}
-  <h2>Red</h2>
-{:else}
-  <h2>Some other color</h2>
-{/if}
-<input type="text" bind:value={colorName} />
-<input type="checkbox" bind:checked={isChecked} />
-<hr />
-{#if isChecked}
-  <h2>You win!!!</h2>
-{/if}
+<ul>
+  {#each colorList as color}
+    <li>{color}</li>
+  {/each}
+</ul>
+
+<input type="text" bind:value={color} />
+<button on:click={addColor}>Add Color</button>
