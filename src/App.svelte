18ff9e9e3bf20f5f3c04887f3e749f1462ccda9e
phptuts <glaserpower@gmail.com>
2022-12-27 15:14:58 -0800

    module2-video18

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,9 @@
 <script>
-  const url = 'https://zenquotes.io/api/random';
-  let promiseQuote = getQuote();
-  async function getQuote() {
-    const response = await fetch(url);
-    const [quote] = await response.json();
-    return quote;
-  }
-
-  function refreshQuote() {
-    promiseQuote = getQuote();
-  }
+  import LifeCycle from './LifeCycle.svelte';
+  let show = false;
 </script>
 
-<h1>Quotes</h1>
-
-{#await promiseQuote}
-  <h2>Awaiting Promise</h2>
-{:then quote}
-  <h2>{quote.q}</h2>
-  <h3>{quote.a}</h3>
-{:catch e}
-  <h2>{e.message}</h2>
-{/await}
-<button on:click={refreshQuote}>New Quote</button>
+<input type="checkbox" bind:checked={show} />
+{#if show}
+  <LifeCycle />
+{/if}
