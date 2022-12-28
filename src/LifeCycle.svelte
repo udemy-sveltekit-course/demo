95ffde58d6737370b73f5e4c2f638388397554f6
phptuts <glaserpower@gmail.com>
2022-12-27 16:07:45 -0800

    module2-video19

--- src/LifeCycle.svelte
+++ src/LifeCycle.svelte
@@ -1,18 +1,30 @@
 <script>
-  import { onMount, onDestroy } from 'svelte';
-  let quote = '';
-  let error = false;
-  let loading = true;
-  onMount(async () => {
-    try {
-      const url = 'https://zenquotes.io/api/random';
-      const response = await fetch(url);
-      const [quoteMessage] = await response.json();
-      quote = quoteMessage.q;
-    } catch (e) {
-      error = true;
+  import { onMount, onDestroy, beforeUpdate, afterUpdate } from 'svelte';
+
+  let numbers = [];
+  let unorderedListElement;
+  let autoScroll = false;
+
+  onMount(() => {
+    const intervalId = setInterval(() => {
+      numbers = [...numbers, numbers.length + 1];
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+      console.log('also destroyed');
+    };
+  });
+
+  beforeUpdate(() => {
+    console.log('before update');
+  });
+
+  afterUpdate(() => {
+    console.log('after update');
+    if (autoScroll) {
+      unorderedListElement.scrollTo(0, unorderedListElement.scrollHeight);
     }
-    loading = false;
   });
 
   onDestroy(() => {
@@ -20,10 +32,18 @@
   });
 </script>
 
-{#if loading}
-  <h2>Loading...</h2>
-{:else if error}
-  <h2>Quote API not working</h2>
-{:else}
-  <h2>{quote}</h2>
-{/if}
+<ul bind:this={unorderedListElement}>
+  {#each numbers as number (number)}
+    <li>{number}</li>
+  {/each}
+</ul>
+
+<label>Auto Scroll <input type="checkbox" bind:checked={autoScroll} /></label>
+
+<style>
+  ul {
+    max-height: 100px;
+    border: solid black 1px;
+    overflow-y: scroll;
+  }
+</style>
