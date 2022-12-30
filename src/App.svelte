76691cc42667f56310e3f69ccc96420803220a8c
phptuts <glaserpower@gmail.com>
2022-12-30 11:44:46 -0800

    module2-video26

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,22 @@
 <script>
   let show = false;
-  function exampleAction(node) {
+  let width = 100;
+  function exampleAction(node, { color, width }) {
     const contextFunc = (e) => {
       e.preventDefault();
       alert('Right Click');
     };
     node.addEventListener('contextmenu', contextFunc);
+    node.style.backgroundColor = color;
+    node.style.width = width + 'px';
     return {
       destroy() {
         console.log('destroy was called');
         node.removeEventListener('contextmenu', contextFunc);
+      },
+      update({ color, width }) {
+        console.log(color, width);
+        node.style.width = width + 'px';
       },
     };
   }
@@ -17,8 +24,9 @@
 
 <input type="checkbox" bind:checked={show} />
 {#if show}
-  <div use:exampleAction />
+  <div use:exampleAction={{ color: '#AA0000', width }} />
 {/if}
+<input type="range" min="50" max="500" bind:value={width} />
 
 <style>
   div {
