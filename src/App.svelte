5dfbdb3d0477fa3f13629029a5f796c02510c1e1
phptuts <glaserpower@gmail.com>
2022-12-29 20:08:31 -0800

    module2-video23

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,8 @@
 <script>
-  import { onDestroy } from 'svelte';
-  import countStore from './stores/count.store';
+  import exStore, { store1 } from './stores/ex.store';
   setTimeout(() => {
-    countStore.set(100);
+    store1.set(100);
   }, 2000);
-
-  setTimeout(() => {
-    countStore.multipleBy(100);
-  }, 3000);
-
-  const unsub = countStore.subscribe((n) => {
-    console.log('count store', n);
-  });
-  onDestroy(() => {
-    unsub();
-  });
-
-  function add() {
-    countStore.addOne();
-  }
-
-  function minus() {
-    countStore.minusOne();
-  }
 </script>
 
-<h1>{$countStore}</h1>
-
-<input type="text" bind:value={$countStore} />
-<div>
-  <button on:click={add}>+</button>
-  <button on:click={minus}>-</button>
-</div>
+<h1>{$exStore}</h1>
