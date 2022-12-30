9fe25a09c606e03b9668470924dc753f65b76612
phptuts <glaserpower@gmail.com>
2022-12-29 18:26:23 -0800

    module2-video21

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,40 @@
 <script>
-  import GeoLocation from './GeoLocation.svelte';
-  let show = false;
+  import { onDestroy } from 'svelte';
+  import countStore from './stores/count.store';
+  setTimeout(() => {
+    countStore.set(100);
+  }, 2000);
+
+  setTimeout(() => {
+    countStore.update((n) => {
+      return n * 100;
+    });
+  }, 3000);
+
+  const unsub = countStore.subscribe((n) => {
+    console.log('count store', n);
+  });
+  onDestroy(() => {
+    unsub();
+  });
+
+  function add() {
+    countStore.update((n) => {
+      return n + 1;
+    });
+  }
+
+  function minus() {
+    countStore.update((n) => {
+      return n - 1;
+    });
+  }
 </script>
 
-<input type="checkbox" bind:checked={show} />
-{#if show}
-  <GeoLocation />
-{/if}
+<h1>{$countStore}</h1>
+
+<input type="text" bind:value={$countStore} />
+<div>
+  <button on:click={add}>+</button>
+  <button on:click={minus}>-</button>
+</div>
