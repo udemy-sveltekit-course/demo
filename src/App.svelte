dea0fbe2e28a8d0573ef84fd64618e35a31424fb
phptuts <glaserpower@gmail.com>
2022-12-30 13:12:13 -0800

    module2-video27

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,12 @@
 <script>
-  let show = false;
-  let width = 100;
-  function exampleAction(node, { color, width }) {
-    const contextFunc = (e) => {
-      e.preventDefault();
-      alert('Right Click');
-    };
-    node.addEventListener('contextmenu', contextFunc);
-    node.style.backgroundColor = color;
-    node.style.width = width + 'px';
-    return {
-      destroy() {
-        console.log('destroy was called');
-        node.removeEventListener('contextmenu', contextFunc);
-      },
-      update({ color, width }) {
-        console.log(color, width);
-        node.style.width = width + 'px';
-      },
-    };
-  }
+  import Box from './Box.svelte';
 </script>
 
-<input type="checkbox" bind:checked={show} />
-{#if show}
-  <div use:exampleAction={{ color: '#AA0000', width }} />
-{/if}
-<input type="range" min="50" max="500" bind:value={width} />
-
-<style>
-  div {
-    width: 300px;
-    height: 300px;
-    border: solid black 1px;
-  }
-</style>
+<Box>
+  <h1 slot="top">Hello World</h1>
+  <h2 slot="middle">Hi</h2>
+  <div slot="bottom">
+    <p>Hello how are you doing?</p>
+    <p>Hi</p>
+  </div>
+</Box>
