8da36ffad95dc3faef2d04ffc0c1cff55bddea04
phptuts <glaserpower@gmail.com>
2022-12-31 16:57:18 -0800

    writable stores

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,33 @@
 <script>
-  import GeoLocation from './GeoLocation.svelte';
-  let show = false;
+  import { onDestroy } from 'svelte';
+  import countStore from './stores/count.store';
+  const unsub = countStore.subscribe((n) => {
+    console.log(n, 'count store');
+  });
+
+  function add() {
+    countStore.update((n) => n + 1);
+  }
+
+  function minus() {
+    countStore.update((n) => n - 1);
+  }
+
+  //   setTimeout(() => {
+  //     countStore.set(2000);
+  //   }, 2000);
+
+  //   setTimeout(() => {
+  //     countStore.update((n) => {
+  //       return n + 300;
+  //     });
+  //   }, 2000);
+
+  onDestroy(() => {
+    unsub();
+  });
 </script>
 
-<input type="checkbox" bind:checked={show} />
-{#if show}
-  <GeoLocation />
-{/if}
+<h1>{$countStore}</h1>
+<button on:click={add}>+</button>
+<button on:click={minus}>-</button>
